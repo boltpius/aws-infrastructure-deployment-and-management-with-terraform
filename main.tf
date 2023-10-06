986197a0f5b1b270015f710b655578929e7d8abd
Pius <127052041+boltpius@users.noreply.github.com>
2023-10-06 07:33:25 +0100

    added variables to my code

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@
 # VPC 
 
 resource "aws_vpc" "piusVPC" {
-  cidr_block       = "10.0.0.0/24"
+  cidr_block       = var.vpc_cidr_block
   instance_tenancy = "default"
 
   tags = {
-    Name = "piusVPC"
+    Name = var.VPCname
   }
 }
 
@@ -17,41 +17,31 @@ resource "aws_internet_gateway" "piusGATEWAY" {
   vpc_id = aws_vpc.piusVPC.id
 
   tags = {
-    Name = "piusGATEWAY"
+    Name = var.IGWname
   }
 }
 
 # 2 PUBLIC SUBNETS AND 1 PRIVATE SUBNET
 
-resource "aws_subnet" "pub1" {
+resource "aws_subnet" "pubsubnets" {
+  count = length(var.public_subnets)
   vpc_id     = aws_vpc.piusVPC.id
-  cidr_block = "10.0.0.0/28"
-  availability_zone = "eu-west-1a"
+  cidr_block = var.subnet_cidr_block[count.index]
+  availability_zone = var.subnet_availability_zone[count.index]
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "pub1"
-  }
-}
-
-resource "aws_subnet" "pub2" {
-  vpc_id     = aws_vpc.piusVPC.id
-  cidr_block = "10.0.0.16/28"
-  availability_zone = "eu-west-1b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "pub2"
+    Name = var.public_subnets[count.index]
   }
 }
 
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.piusVPC.id
-  cidr_block = "10.0.0.32/28"
-  availability_zone = "eu-west-1c"
+  cidr_block = var.privatesub_cidr_block
+  availability_zone = var.privatesub_availability_zone 
 
   tags = {
-    Name = "private"
+    Name = var.private_subnet
   }
 }
 
@@ -59,6 +49,8 @@ resource "aws_subnet" "private" {
 
 
 resource "aws_route_table" "piustable" {
+
+  count = length(var.routetable)
   vpc_id = aws_vpc.piusVPC.id
 
   route {
@@ -67,39 +59,24 @@ resource "aws_route_table" "piustable" {
   }
 
   tags = {
-    Name = "piustable"
+    Name = var.routetable[count.index]
   }
 }
 resource "aws_route_table_association" "piusroute" {
-  subnet_id      = aws_subnet.pub1.id
-  route_table_id = aws_route_table.piustable.id
-}
-
-resource "aws_route_table" "piustable2" {
-  vpc_id = aws_vpc.piusVPC.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.piusGATEWAY.id
-  }
-
-  tags = {
-    Name = "piustable2"
-  }
-}
-resource "aws_route_table_association" "piusroute2" {
-  subnet_id      = aws_subnet.pub2.id
-  route_table_id = aws_route_table.piustable2.id
+  count = length(var.public_subnets)
+  subnet_id      = aws_subnet.pubsubnets[count.index].id
+  route_table_id = aws_route_table.piustable[count.index].id
+ # route_table_id = element(aws_route_table.piustable.*.id, count.index)
 }
 
 # SECURITY GROUP FOR HTTPS, HTTP AND SSH
 
 resource "aws_security_group" "pius_secuirty" {
-  name        = "Pius Security"
-  description = "Allow TLS, SSH, HTTP inbound traffic"
+  name        = var.security_name
+  description = var.security_description
   vpc_id      = aws_vpc.piusVPC.id
 
-
+  
 ingress {
     description      = "ALLOW HTTP from ANYWHERE"
     from_port        = 80
@@ -109,7 +86,7 @@ ingress {
   }
 
   ingress {
-    description      = "TLS from ANYWHERE"
+    description      = "HTTPS (TLS) from ANYWHERE"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
@@ -133,20 +110,20 @@ ingress {
   }
 
   tags = {
-    Name = "allow_tls_ssh_http"
+    Name = var.security_tags
   }
 }
 
 # LAUNCH TEMPLATE
 
 resource "aws_launch_template" "piustemplate" {
-  name = "piustemplate"
+  name = var.template_name
 
-  image_id = "ami-01dd271720c1ba44f"
+  image_id = var.template_image_id
 
-  instance_type = "t2.micro"
+  instance_type = var.template_instance_type
 
-  key_name = "yes"
+  key_name = var.template_private_key
 
   vpc_security_group_ids = [aws_security_group.pius_secuirty.id]
 
@@ -154,20 +131,20 @@ resource "aws_launch_template" "piustemplate" {
     resource_type = "instance"
 
     tags = {
-      Name = "pius application"
+      Name = var.template_tags
     }
   }
 
-  user_data = filebase64("userdata.sh")
+  user_data = filebase64(var.template_userdata)
 }
 
 # AUTOSCALING GROUP ASG
 
 resource "aws_autoscaling_group" "piusautoscaling" {
-  vpc_zone_identifier       = [aws_subnet.pub1.id, aws_subnet.pub2.id]
-  desired_capacity   = 0
-  max_size           = 0
-  min_size           = 0
+  vpc_zone_identifier       = [for subnet in aws_subnet.pubsubnets : subnet.id]  
+  desired_capacity   = var.asg_desired_capacity
+  max_size           = var.asg_max_size
+  min_size           = var.asg_min_size
 
   launch_template {
     id      = aws_launch_template.piustemplate.id
@@ -178,16 +155,16 @@ resource "aws_autoscaling_group" "piusautoscaling" {
 # ACM CERTIFICATE AND VALIDATION WITH ROUTE53 
 
 resource "aws_acm_certificate" "piuscert" {
-  domain_name       = "satar.piustech.io"
+  domain_name       = var.acm_domain_name
   validation_method = "DNS"
 
   tags = {
-    Environment = "piusacmcert"
+    Environment = var.acm_tags
   }
 }
 
 data "aws_route53_zone" "piustech" { # This sends aws route53 to retrieve information about the dns zoned named piustech.io 
-  name         = "piustech.io" # to match the domain used in the ACM CERTIFICATE
+  name         = var.data_name # to match the domain used in the ACM CERTIFICATE
   private_zone = false # it is set to false because my dns zone is public 
  }
 
@@ -216,22 +193,21 @@ resource "aws_acm_certificate_validation" "piusvalidation" {  # this block uses 
 
 # APPLICATION LOADBALANCER (ALB), TAGRET GROUP AND LISTENER 
 resource "aws_lb" "piusalb" {
-  name               = "piusloadbalancer"
+  name               = var.alb_name
   internal           = false
-  load_balancer_type = "application"
+  load_balancer_type = var.load_balancer_type
   security_groups    = [aws_security_group.pius_secuirty.id]
-  subnets            = [aws_subnet.pub1.id, aws_subnet.pub2.id]
-  # this was the previous one subnets            = [for subnet in aws_subnet.public : subnet.id]
+  subnets            =  [for subnet in aws_subnet.pubsubnets : subnet.id]
 
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
   tags = {
-    Environment = "production"
+    Environment = var.alb_tags
   }
 }
 
 resource "aws_lb_target_group" "piustargetgroup" {
-  name     = "pius-lb-targetgroup"
+  name     = var.targetgroup_name
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.piusVPC.id
@@ -239,9 +215,9 @@ resource "aws_lb_target_group" "piustargetgroup" {
 
 resource "aws_lb_listener" "piuslistener" {
   load_balancer_arn = aws_lb.piusalb.arn
-  port              = "443"
-  protocol          = "HTTPS" 
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  port              = var.lb_listener_port
+  protocol          = var.lb_listener_protocol
+  ssl_policy        = var.lb_listener_sslpolicy
   certificate_arn   = aws_acm_certificate.piuscert.arn
 
   default_action {
@@ -252,6 +228,7 @@ resource "aws_lb_listener" "piuslistener" {
 
 # REDIRECT HTTP TRAFFIC TO HTTPS AND THEN FORWARD THE TRAFFIC TO LB TARGET GROUP
 resource "aws_lb_listener" "piusredirect" {
+  
   load_balancer_arn = aws_lb.piusalb.arn
   port              = "80"
   protocol          = "HTTP"
@@ -270,10 +247,10 @@ resource "aws_lb_listener" "piusredirect" {
 # CREATING A ROUTE 53 RECORD FOR THE DNS AND ALIASING IT WITH THE LOADBALANCER
 
 resource "aws_route53_record" "satar_piustech" {
-  # zone_id = aws_route53_zone.piustech.zone_id
   zone_id = data.aws_route53_zone.piustech.zone_id
-  name    = "satar.piustech.io"
+  name    = var.route_53dns_recordname 
   type    = "A"
+  allow_overwrite = true
 
   alias {
     name                   = aws_lb.piusalb.dns_name
@@ -282,7 +259,7 @@ resource "aws_route53_record" "satar_piustech" {
   }
 }
 
-# ALB TARGET GROUP ATTACHMENT 
+# ALB TARGET GROUP ATTACHMENT to autoscaling group
 resource "aws_autoscaling_attachment" "piusattachment_ASG" {
   autoscaling_group_name = aws_autoscaling_group.piusautoscaling.id
   lb_target_group_arn    = aws_lb_target_group.piustargetgroup.arn
